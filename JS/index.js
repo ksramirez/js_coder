61900af45a385a39afa6db41c46124373d0e37f9
SolRamirez <ksoleramirez@gmail.com>
2022-08-03 20:07:32 -0300

    Primer entrega Proyecto final

--- JS/index.js
+++ JS/index.js
@@ -1,56 +1,8 @@
 
-/*
-const eleccionAroma = prompt('Elija un aroma de la lista : citrico, frutal, floral').toLowerCase()
+//Array Carrito
+let carrito= []
 
-if (eleccionAroma == 'citrico') {
-    alert('Los aromas cítricos Puede ayudar a despejar la mente en estados de «agobio» o mucha actividad intelectual. Se le considera también estimulante y energizante en estados de fatiga.')
-} else if (eleccionAroma == 'frutal') {
-    alert('Los aromas como el limón, toronja, mandarina y naranja son revitalizantes, una descarga de energía a los sentidos que puede neutralizar el estrés, crear una atmósfera agradable e influir en la percepción del lugar, ya que suelen asociarse con la limpieza y favorecen la concentración.')
-} else if (eleccionAroma == 'floral') {
-    alert('El aroma floral es el más popular y extenso de todas las familias. En concreto, las variedades más utilizadas son la rosa y el jazmín, que se encuentran en el corazón de la mayoría de creaciones de perfumes.')
-} else {
-    alert('El aroma seleccionado no se encuentra en stock por el momento. Ingrese un aroma en Stock.')
-}
-*/
-
-
-/*const ingreseNombre = prompt('Ingrese su Nombre').toLowerCase()
-
-let ingreseNumero = Number(prompt('Ingresar Numero'));
-
-
-for (let i = 1; i <= 10 ; i++) {
-    let resultado = ingreseNumero + i ;
-    console.log(ingreseNumero +" + " + i +" = "+ resultado);
-}*/
-
-
-//SIMULADOR INTERACTIVO
-
-/* const calcularCuotas= (montoTotal, cantidadCuotas) =>
-{
-    return montoTotal/cantidadCuotas
-}
-
-let montoTotal= Number(prompt('Por favor ingrese el monto TOTAL de su saldo'))
-
-let cantidadCuotas= Number(prompt('Ingrese en cuantas cuotas quiere realizar el pago : 6, 12 o 24 CUOTAS'))
-let resultado= calcularCuotas (montoTotal,cantidadCuotas)
-
-
-if (cantidadCuotas==6) {
- alert('Usted seleccionó 6 cuotas de :  $' + resultado);
-} else if (cantidadCuotas==12) {
-    alert('Usted seleccionó 12 cuotas de :  $' + resultado);
-}else if (cantidadCuotas==24) {
-    alert('Usted seleccionó 24 cuotas de :  $' + resultado);
-}else {
-    alert ('La cantidad de cuotas ingesadas no está habilitada, pruebe nuevamente.')
-}
-
- */
-
-
+//Productos
 class Producto {
     constructor(prod, aroma, modelo, descripcion, precio) {
         this.pord = prod
@@ -60,26 +12,91 @@ class Producto {
         this.precio = precio
     }
 
-    mostrarAroma() {
-        console.log('El aroma del producto seleccionado es: ' + this.aroma)
-    }
 }
 
-const producto1 = new Producto('Vela', 'Vainilla', 'Minimal', 'Vela de cera de soja artesanal', 2500)
-const producto2 = new Producto('Vela', 'Frutilla', 'Ceramica', 'Vela de cera de soja artesanal', 2800)
-const producto3= new Producto('Difusor', 'Bebé', 'Home Deco', 'Difusor de varillas en recipiente de vidrio', 3000)
-const producto4 = new Producto('Difusor', 'Fresias', 'Minimal', 'Difusor de varillas en recipiente de cerámica', 3500)
-const producto5 = new Producto('Perlas aromáticas', 'Cher', 'Minimal', 'Perlas de cerámica para perfumar ambientes', 2000)
-
-console.log(producto4)
-producto4.mostrarAroma()
-
+const producto1 = new Producto('VelaMinimal', ['Vainilla','Frutos Rojos','Lavanda','Pop Corn','Jazmín'], 'Minimal', 'Vela de cera de soja artesanal', 2500)
+const producto2 = new Producto('VelaCeramica', ['Frutilla','Flores Blancas','Frambuesa Melón','Frambuesa y Pimienta Rosa'], 'Ceramica', 'Vela de cera de soja artesanal', 2800)
+const producto3= new Producto('DifusorVidrio', ['Bebé','Wanama','Vainilla Caramelo', 'Lemon Grass', 'Fresias', 'Nag Champa'], 'Home Deco', 'Difusor de varillas en recipiente de vidrio', 3000)
+const producto4 = new Producto('DifusorCeramica', ['Bebé','Wanama','Vainilla Caramelo', 'Lemon Grass', 'Fresias', 'Nag Champa'], 'Minimal', 'Difusor de varillas en recipiente de cerámica', 3500)
+const producto5 = new Producto('PerlasAromaticas', ['Cher','Wanama','Caricias de Algodón'], 'Minimal', 'Perlas de cerámica para perfumar ambientes y roperos', 2000)
 
 
 const productos = [producto1, producto2, producto3, producto4, producto5]
-console.log(productos)
 
 
-for (const producto of productos) {
-    console.log(producto.descripcion, producto.precio)
+
+
+
+ //Elección de Características del Producto
+const editarProductoSelec = (prodSeleccionado) => {
+
+    const productoElegido = {
+        prod: prodSeleccionado.prod,
+        descripcion: prodSeleccionado.descripcion,
+        mod: prodSeleccionado.modelo,
+        fragancia: '',
+        cantidad: 1,
+        precio: prodSeleccionado.precio,
+    }
+
+    productoElegido.fragancia = prompt('Por Favor seleccione un aroma de la lista:  ' + prodSeleccionado.aroma.join(' - ')).toLowerCase()
+    productoElegido.cantidad = Number(prompt('Ingrese cuántas unidades desea: '))
+
+    return productoElegido
+} 
+
+//Elección de producto
+const seleccionarProducto = () => {
+    const seleccionUsuario = prompt('Seleccione el producto que le interese: VelaMinimal, VelaCeramica, DifusorVidrio, DifusorCeramica, PerlasAromaticas').toLowerCase()
+
+    switch (seleccionUsuario) {
+        case 'velaminimal':
+            console.log('Seleccionaste : Vela Minimal')
+            carrito.push(editarProductoSelec(producto1))
+            break
+        case 'velaceramica':
+            console.log('Seleccionaste : Vela Ceramica')
+            carrito.push(editarProductoSelec(producto2))
+            break
+        case 'difusorvidrio':
+            console.log('Seleccionaste : Difusor de vidrio')
+            carrito.push(editarProductoSelec(producto3))
+            break
+        case 'difusorceramica':
+            console.log('Seleccionaste : Difusor de Cerámica')
+            carrito.push(editarProductoSelec(producto4))
+            break
+        case 'perlasaromaticas':
+            console.log('Seleccionaste : Perlas Aromaticas')
+            carrito.push(editarProductoSelec(producto5))
+            break
+
+        default:
+            console.log('Por favor, elegi un modelo dentro de las opciones')
+            break
+    }
+
+    if (confirm('Desea agregar otro producto a su compra?')) {
+        seleccionarProducto()
+    }
 }
+
+seleccionarProducto()
+
+
+//Carrito Total
+
+const TotalCarrito = () => {
+    let sumaTotalCarrito = 0
+    for (const producto of carrito) {
+        sumaTotalCarrito += producto.precio * producto.cantidad
+    }
+    return sumaTotalCarrito
+}
+console.log(carrito);
+
+
+
+alert('Gracias por su compra, su total es de $' + TotalCarrito())
+console.log('Gracias por su compra, su total es de $' + TotalCarrito())
+
